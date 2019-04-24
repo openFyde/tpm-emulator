1ad74ec816215bfda4bb089c6d7954d4ef51417c
yang <yang@fydeos.io>
2019-04-24 18:27:30 +0800

    fixed tpm issue

--- tpmd_dev/linux/tpmd_dev.c
+++ tpmd_dev/linux/tpmd_dev.c
@@ -9,16 +9,12 @@
 #include <linux/version.h>
 #include "tpm_tis_core.h"
 #include "config.h"
-#define DEBUG 1
 
 #define CLASS_NAME "tpmd_dev"
 #define TPM_DEVICE_MINOR  MISC_DYNAMIC_MINOR
 #define TPM_DEVICE_ID  "vtpm"
 #define TPM_READY_SIG "ready"
-/*
-#define TPM_TAG_RSP_COMMAND             0x00C4
-#define TPM_ORD_GET_CAP 101
-*/
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mario Strasser <mast@gmx.net>, Yang Tsao <yang@flintos.io>");
 MODULE_DESCRIPTION("Trusted Platform Module (TPM) Emulator");
@@ -32,17 +28,6 @@ MODULE_PARM_DESC(tpmd_socket_name, " Sets the name of the TPM daemon socket.");
                         CLASS_NAME, __FILE__, __LINE__, ## __VA_ARGS__)
 #define info(fmt, ...)  printk(KERN_INFO "%s %s:%d: Info: " fmt "\n", \
                         CLASS_NAME, __FILE__, __LINE__, ## __VA_ARGS__)
-/*
-enum tpm_const_fydeos {
-    TPM_TIMEOUT_A		= 750,
-	TPM_TIMEOUT_B		= 2000,
-	TPM_TIMEOUT_C		= 200,
-	TPM_TIMEOUT_D		= 30,
-    TPM_DURATION_SHORT	= 20,
-	TPM_DURATION_MEDIUM	= 750,
-	TPM_DURATION_LONG	= 2000,
-};
-*/
 struct tpm_emulator_phy {
     struct socket *tpmd_sock;
     struct mutex emulator_mutex;
@@ -53,20 +38,7 @@ struct tpm_emulator_phy {
     u16 header_index;
     u16 tail_index;
 };
-/*
-struct tpm_command_get_prop {
-  struct tpm_input_header header;
-  __be32 cmd_type;
-  __be32 space;
-  __be32 prop;  
-};
 
-struct tpm_command_ret_prop {
-  struct tpm_output_header header;
-  __be32 space;
-  cap_t cap;
-};
-*/
 u8 _status = 0x0;
 u8 _access = 0x0;
 
@@ -195,76 +167,25 @@ static int fetch_data(u8 *out, u16 len) {
 }
 
 static void on_handle_begin(void) {
-  dev_info(phy->dev, "on_handle_begin");
+  //dev_info(phy->dev, "on_handle_begin");
   unset_command_ready();
 }
 
 static void on_handle_send_complete(void){
-  dev_info(phy->dev, "on_handle_send_complete");
+  //dev_info(phy->dev, "on_handle_send_complete");
   reset_buf();
 }
 
 static void on_handle_recv_complete(void) {
-  dev_info(phy->dev, "on_handle_recv_complete");
+  //dev_info(phy->dev, "on_handle_recv_complete");
   set_data_avalible();
 }
 
 static void on_handle_error(void) {
-  dev_info(phy->dev, "on_handle_error");
+  //dev_info(phy->dev, "on_handle_error");
   reset_phy();
 }
-/*
-static bool is_request_cap(void) {
-  const struct tpm_command_get_prop *cmd = (const struct tpm_command_get_prop *) phy->buf;
-  if (get_buf_length() < TPM_HEADER_SIZE + 12)
-      return false;
-  return cmd->header.tag == cpu_to_be16(TPM_TAG_RQU_COMMAND) &&
-      cmd->header.ordinal == cpu_to_be32(TPM_ORD_GET_CAP) &&
-      cmd->cmd_type == cpu_to_be32(TPM_CAP_PROP);
-}
 
-static u32 get_req_cmd(void) {
-  const struct tpm_command_get_prop *cmd = (const struct tpm_command_get_prop *)phy->buf;
-  return be32_to_cpu(cmd->prop);
-}
-
-static bool can_handle_prop_req(u32 req) {
-  return req == TPM_CAP_PROP_TIS_TIMEOUT || 
-           req == TPM_CAP_PROP_TIS_DURATION;
-}
-
-
-static int handle_prop_req(u32 req) {
-  struct tpm_command_ret_prop *buf =(struct tpm_command_ret_prop *) phy->buf;
-  size_t length;
-  on_handle_begin();
-  on_handle_send_complete();
-  buf->header.tag = cpu_to_be16(TPM_TAG_RSP_COMMAND);
-  switch (req) {
-    case TPM_CAP_PROP_TIS_TIMEOUT:
-      buf->cap.timeout.a = cpu_to_be32(TPM_TIMEOUT_A);
-      buf->cap.timeout.b = cpu_to_be32(TPM_TIMEOUT_B);
-      buf->cap.timeout.c = cpu_to_be32(TPM_TIMEOUT_C);
-      buf->cap.timeout.d = cpu_to_be32(TPM_TIMEOUT_D);
-      break;
-    case TPM_CAP_PROP_TIS_DURATION:
-      buf->cap.duration.tpm_short = cpu_to_be32(TPM_DURATION_SHORT);
-      buf->cap.duration.tpm_medium = cpu_to_be32(TPM_DURATION_MEDIUM);
-      buf->cap.duration.tpm_long = cpu_to_be32(TPM_DURATION_LONG);
-      break;
-    default:
-	  on_handle_error();
-	  return -1;
-  }
-  length = sizeof(*buf);
-  buf->header.length = cpu_to_be32(length);
-  buf->header.return_code = 0;
-  dev_info(phy->dev, "return length:%lu", length);
-  phy->tail_index = length; 
-  on_handle_recv_complete();
-  return 0; 
-}
-*/
 static int tpmd_handle_command(void){
   int res;
   mm_segment_t oldmm;
@@ -294,11 +215,7 @@ static int tpmd_handle_command(void){
   iov.iov_base = (void*)phy->buf;
   iov.iov_len = TPM_CMD_BUF_SIZE;
   iov_iter_init(&(msg.msg_iter), READ | ITER_IOVEC, &iov, 1, iov.iov_len);
-  res = sock_recvmsg(phy->tpmd_sock, &msg,
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 7, 0)
-                     TPM_CMD_BUF_SIZE,
-#endif
-                     0);
+  res = sock_recvmsg(phy->tpmd_sock, &msg, 0);
   set_fs(oldmm);
   if (res < 0) {
     error("sock_recvmsg() failed: %d\n", res);
@@ -312,68 +229,10 @@ on_error:
   on_handle_error();
   return res;
 }
-/*
-static int parse_command_and_excute(u32 addr, u16 len, u8 *buffer, bool is_read) {
-  if (!phy) return -1;
-  //dev_dbg(phy->dev, "get command:addr:(%x),len:%d,isread:%d", addr, len, is_read);
-  if (len == 1){
-    // read/write 8bit
-    if(is_read) {
-      if (addr == TPM_ACCESS(phy->priv.locality)){ // get access property;
-        *buffer = get_access_prop();
- //       dev_dbg(phy->dev, "return access status:(%x)", *buffer);
-        return 0;
-      }
-      if (addr == TPM_STS(phy->priv.locality)) { // get status;
-        *buffer = get_status_prop();
-  //      dev_dbg(phy->dev, "return status:(%x)", *buffer);
-        return 0;
-      }
-      if (addr == TPM_RID(phy->priv.locality)) { // get release id;
-        *buffer = get_rid();
-  //      dev_dbg(phy->dev, "return rid:(%x)", *buffer);
-        return 0;
-      }
 
-    } else {
-      if (addr == TPM_STS(phy->priv.locality)) {
-        if (*buffer == TPM_STS_COMMAND_READY){ // reset system;
-          reset_phy();
-          return 0;
-        }
-        if (*buffer == TPM_STS_GO){ // excute the command stored in buf
-          return tpmd_handle_command();
-        }
-      }
-      if (addr == TPM_ACCESS(phy->priv.locality)){
-        if (*buffer == TPM_ACCESS_ACTIVE_LOCALITY) {
-            reset_phy();
-            inactive_locality();
-            return 0;
-        }
-      }
-
-    }
-  }
-  if (addr == TPM_DATA_FIFO(phy->priv.locality)) {
-    if (is_read) {
-      return fetch_data(buffer, len);
-    }else {
-      return push_data(buffer, len);
-    }
-  } else {
-    if (is_read) {
-      memset(buffer,0 ,len);
-      return 0;
-    }else {
-      return 0;
-    }
-  }
-}
-*/
 static int read_and_excute(u32 addr, u16 len, u8 *buffer) { 
   if (!phy) return -1;
-    dev_dbg(phy->dev, "get read command:addr:(%x),len:%d", addr, len);
+ //   dev_dbg(phy->dev, "get read command:addr:(%x),len:%d", addr, len);
   if (len == 1){
     if (addr == TPM_ACCESS(phy->priv.locality)){ // get access property;
       *buffer = get_access_prop();
@@ -401,7 +260,7 @@ static int read_and_excute(u32 addr, u16 len, u8 *buffer) {
 
 static int write_and_excute(u32 addr, u16 len, const u8 *buffer) {
   if (!phy) return -1;
-  dev_dbg(phy->dev, "get write command:addr:(%x),len:%d", addr, len);
+  //dev_dbg(phy->dev, "get write command:addr:(%x),len:%d", addr, len);
   if (len == 1){
     if (addr == TPM_STS(phy->priv.locality)) {
       if (*buffer == TPM_STS_COMMAND_READY){ // reset system;
@@ -409,13 +268,6 @@ static int write_and_excute(u32 addr, u16 len, const u8 *buffer) {
           return 0;
       }
       if (*buffer == TPM_STS_GO){ // excute the command stored in buf
-          /*
-        if (is_request_cap()) {
-			u32 req = get_req_cmd();
-            dev_info(phy->dev,"get cap received:%u", req);
-			if (can_handle_prop_req(req))
-				return handle_prop_req(req);
-		}*/
         return tpmd_handle_command();
       }
     }
